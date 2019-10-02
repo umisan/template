466e760a124c8b02bc45ae0ea70241d56952eebd
umisan <t.umino.nit@gmail.com>
2019-10-02 15:21:47 +0900

    add template

---
+++ template.cpp
@@ -0,0 +1,21 @@
+//競技プログラミング用のテンプレート
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <math.h>
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+
+using namespace std;
+using ll = long long int;
+using P = pair<int, int>;
+using PL = pair<ll, ll>;
+using V = vector<int>;
+using VL = vector<ll>;
+
+int main()
+{
+
+}
