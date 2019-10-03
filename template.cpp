9357c68945c6084abbaf8898f7d77bf255d2d908
umisan <t.umino.nit@gmail.com>
2019-10-03 13:54:45 +0900

    add definitions

--- template.cpp
+++ template.cpp
@@ -4,16 +4,19 @@
 #include <algorithm>
 #include <map>
 #include <queue>
+#include <string>
 #include <math.h>
 
-#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
 
 using namespace std;
 using ll = long long int;
-using P = pair<int, int>;
-using PL = pair<ll, ll>;
-using V = vector<int>;
-using VL = vector<ll>;
+using p = pair<int, int>;
+using pl = pair<ll, ll>;
+using v = vector<int>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vl = vector<ll>;
 
 int main()
 {
