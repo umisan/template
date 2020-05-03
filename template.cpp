94b44070f05510af34ba2087af009b9c87a73263
umisan <t.umisan6@gmail.com>
2020-05-04 03:09:30 +0900

    template修正

--- template.cpp
+++ template.cpp
@@ -7,11 +7,13 @@
 #include <string>
 #include <math.h>
 #include <stack>
+#include <limits>
 
 #define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
 #define repr(i, n) for(int i = n - 1; i >= 0; i--)
 
 using namespace std;
+//エイリアス
 using ll = long long int;
 using p = pair<int, int>;
 using pl = pair<ll, ll>;
@@ -20,7 +22,12 @@ using vd = vector<double>;
 using vs = vector<string>;
 using vl = vector<ll>;
 
+//定数
+const int intmax = numeric_limits<int>::max();
+const ll llmax = numeric_limits<ll>::max();
+const ll mod = 1e9 + 7;
+const double pi = M_PI;
+
 int main()
 {
-    repr(i, 30) cout << i << " ";
 }
